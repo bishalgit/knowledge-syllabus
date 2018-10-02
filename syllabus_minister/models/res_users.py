8ef6fc4fb644ea25a7bc3ae0e0f0f40b69026a03
Jharana Gurung <jharna.915@gmail.com>
2018-10-02 17:24:02 +0545

    [ADD] syllabus_expert_group: scaffold module

--- syllabus_minister/models/res_users.py
+++ syllabus_minister/models/res_users.py
@@ -11,16 +11,16 @@ class ResUsers(models.Model):
 
     rpn = fields.Integer('RPN', help="This is the role priority number of the user.")
 
-    @api.model
-    def create(self, vals):
-        user = super(ResUsers, self).create(vals)
-        _logger.warning("User ID " + str(user.id))
-        _logger.warning("User Role Name " + str(user.role_ids))
-        
-        # to assure that the user creator in from university group
-        if not self.env.user.id == 1:
-            user.write({
-                'rpn': 1
-            })
-        return user
+    # @api.model
+    # def create(self, vals):
+    #     user = super(ResUsers, self).create(vals)
+    #     _logger.warning("User ID " + str(user.id))
+    #     _logger.warning("User Role Name " + str(user.role_ids))
+    #
+    #     # to assure that the user creator in from university group
+    #     if not self.env.user.id == 1:
+    #         user.write({
+    #             'rpn': 1
+    #         })
+    #     return user
 
