56f1ad905226ac174ceb48eb342bc5999d3faf38
Ashim Bajracharya <ashimbazracharya@gmail.com>
2018-10-07 15:39:50 +0545

    [ADD] syllabus_college: add filter to select the university for colleges

--- syllabus_college/models/__init__.py
+++ syllabus_college/models/__init__.py
@@ -4,3 +4,4 @@ from . import models
 from . import package
 from . import syllabus
 from . import college
+from . import university
--- syllabus_college/models/college.py
+++ syllabus_college/models/college.py
@@ -8,3 +8,19 @@ class College(models.Model):
     _inherit = 'mail.thread'
 
     name = fields.Char(string='Name')
+    packages_id = fields.Many2many('syllabus_college.package', string='Programs')
+
+
+    university_id = fields.Many2one('syllabus_minister.university', string='University', domain="['|', ('user_ids', '=', uid), ('group_ids.users.id', '=', uid)]")
+
+    # Groups Involved in College
+    group_ids = fields.Many2many('res.groups', string="Related Groups")
+
+    @api.model
+    def create(self, vals):
+        college = super(College, self).create(vals)
+        college.write({
+            'group_ids': [(4, self.env.ref('syllabus_minister.syllabus_minister_group_administrator').id),
+                          (4, self.env.ref('syllabus_minister.syllabus_minister_group_university').id)]
+        })
+        return college
