8bc863891dc329702fe60acb9bb98b80ff5cd809
Jharana Gurung <jharna.915@gmail.com>
2018-10-08 14:08:46 +0545

    [REF] syllabus_minister: added display field in unit

--- syllabus_minister/models/unit.py
+++ syllabus_minister/models/unit.py
@@ -13,3 +13,9 @@ class Unit(models.Model):
     description = fields.Html(string='Description')
     course_id = fields.Many2one('syllabus_minister.course',string='Course')
     sequence = fields.Integer(string='sequence',default=1)
+    display_name = fields.Char(string='Display Name', compute='_display_name')
+
+    @api.depends('name','course_id')
+    def _display_name(self):
+        for r in self:
+            r.display_name = r.course_id.name + ", " + r.name 
