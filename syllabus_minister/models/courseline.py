bd9bd416320e1483fcea0c54e2fa08dc43d5a26a
Ashim Bajracharya <ashimbazracharya@gmail.com>
2018-10-12 12:27:57 +0545

    [REF] syllabus_minister: provide domain
    Domain is provided such that the courseline of one
    university cannot be accessed by another university.

--- syllabus_minister/models/courseline.py
+++ syllabus_minister/models/courseline.py
@@ -10,7 +10,28 @@ class Courseline(models.Model):
 
     name = fields.Char(string='Name')
     semester = fields.Char(string='Semester')
-    course_id = fields.Many2one('syllabus_minister.course', string='Related Course')
+    course_id = fields.Many2one('syllabus_minister.course', string='Related Course',
+    domain="['|', ('faculty_id.university_id.university_user_ids', '=', uid), ('group_ids.users.id', '=', uid)]")
     sequence = fields.Integer(string='sequence',default=1)
-    program_id = fields.Many2one('syllabus_minister.program',string="Program")
-    syllabus_id = fields.Many2one('syllabus_minister.syllabus',string="Syllabus")
+    program_id = fields.Many2one('syllabus_minister.program',string="Program",
+    domain="['|', ('faculty_id.university_id.university_user_ids', '=', uid), ('group_ids.users.id', '=', uid)]")
+    syllabus_id = fields.Many2one('syllabus_minister.syllabus',string="Syllabus",
+    domain="['|', ('course_id.faculty_id.university_id.university_user_ids', '=', uid), ('group_ids.users.id', '=', uid)]")
+
+    # Groups Involved in Courseline
+    group_ids = fields.Many2many('res.groups', string="Related Groups")
+
+    @api.model
+    def create(self, vals):
+        courseline = super(Courseline, self).create(vals)
+        course.write({
+            'group_ids': [(4, self.env.ref('syllabus_minister.syllabus_minister_group_administrator').id)]
+        })
+        return course
+    
+    # This function filters the courseline record for the program of certain university.
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0, limit=None, order=None):
+        domain = (domain or []) + ['|', ('program_id.faculty_id.university_id.name', '=', self.env.user.university_id.name), ('group_ids.users.id', '=', self.env.uid)]
+        return super(Courseline, self).search_read(domain=domain, fields=fields, offset=offset, limit=limit,
+                                                     order=order)
