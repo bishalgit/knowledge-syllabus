5c681c753d9b2f23c19d8eac2331cafca09072c1
Ashim Bajracharya <ashimbazracharya@gmail.com>
2018-10-28 12:19:46 +0545

    [ADD] syllabus_syllabus: add module

---
+++ syllabus_syllabus/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Syllabus",
+
+    'summary': """
+        Syllabus of the Program""",
+
+    'description': """
+        This module simply generates the syllabus of the certain course of the program
+        of the University.
+    """,
+
+    'author': "Nerku",
+    'website': "http://www.nerku.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Syllabus',
+    'version': '11.0.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'syllabus_minister', 'document_page_approval', 'knowledge'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+        'views/document_page.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ syllabus_syllabus/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import document_page_history
+from . import document_page
