0fd69ddd6aead3d5159bf83f0e1a569576032d50
Ashim Bajracharya <ashimbazracharya@gmail.com>
2018-11-27 11:45:19 +0545

    [FIX] syllabus_minister: fix course type in program old versions

--- syllabus_minister/models/course_type.py
+++ syllabus_minister/models/course_type.py
@@ -12,13 +12,11 @@ class CourseType(models.Model):
 
     name = fields.Char(string='Course Type')
     course_ids = fields.One2many('syllabus_minister.course','course_type', string="Course")
-    total_cr_hr = fields.Integer(_compute='_compute_cr_hr',store=True)
+    # total_cr_hr = fields.Integer(_compute='_compute_cr_hr',store=True)
 
-    def _compute_cr_hr(self):
-        tc = 0
-        for record in self:
-            for r in record.course_ids:
-                tc += r.credit_hours
-                _logger.warning('TC' + str(tc))
-                _logger.warning('TCcccccccccccccccccccc' + str(r.credit_hours))
-            record.total_cr_hr = tc
+    # def _compute_cr_hr(self):
+    #     tc = 0
+    #     for record in self:
+    #         for r in record.course_ids:
+    #             tc += r.credit_hours
+    #         record.total_cr_hr = tc
